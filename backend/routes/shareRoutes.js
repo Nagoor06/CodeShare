cf5332e283ddb625fbd4561077c47b45a5e7c210
Nagoor06 <cb.en.u4cse20269@cb.students.amrita.edu>
2025-12-26 00:52:28 +0530

    Updated Backend logic to handle file and text properly

--- backend/routes/shareRoutes.js
+++ backend/routes/shareRoutes.js
@@ -3,10 +3,9 @@ import multer from "multer";
 import { createShare, openShare } from "../controllers/shareController.js";
 
 const router = express.Router();
-
 const upload = multer({ limits: { fileSize: 5 * 1024 * 1024 } });
-router.post("/", upload.single("file"), createShare);
 
+router.post("/", upload.single("file"), createShare);
 router.post("/open", openShare);
 
 export default router;
